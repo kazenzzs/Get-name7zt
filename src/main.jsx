fcd47849821e3d998274f7c4cdcfe9c996691d03
kazenzzs <117540295+kazenzzs@users.noreply.github.com>
2025-07-19 22:35:11 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+// Optional: Auto enable dark mode if system prefers
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
